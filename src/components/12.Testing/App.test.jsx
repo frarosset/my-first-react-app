35c4aeb1db27b5a6ed6552d3351c89f1b5a0a2f3
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-07 09:19:23 +0100

    test: add sample test to test the configuration of RTL

--- src/components/12.Testing/App.test.jsx
+++ src/components/12.Testing/App.test.jsx
@@ -1,11 +1,25 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect } from "vitest";
 
-describe('something truthy and falsy', () => {
-  it('true to be true', () => {
+import { render, screen } from "@testing-library/react";
+
+import App from "../../App";
+
+describe("something truthy and falsy", () => {
+  it("true to be true", () => {
     expect(true).toBe(true);
   });
 
-  it('false to be false', () => {
+  it("false to be false", () => {
     expect(false).toBe(false);
+  });
+});
+
+describe("App", () => {
+  it("renders headline", () => {
+    render(<App title="React" />);
+
+    screen.debug();
+
+    // check if App components renders headline
   });
 });
